e4ee32ac6c26cabb9ee0b34a5bc3ff700ab1bdaa
“Alessandro <clocchiatti.alessandro@educ.di.unito.it>
2019-08-25 22:21:18 +0200

    Modificata activity TransactionDetail, in base al tipo cambia il colore del bordo. Aggiunto listener btn_back (torna all'activity precedente

--- app/src/main/java/com/example/aleclock/budgetmanager/TransactionDetailActivity.kt
+++ app/src/main/java/com/example/aleclock/budgetmanager/TransactionDetailActivity.kt
@@ -3,15 +3,15 @@ package com.example.aleclock.budgetmanager
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
+import android.widget.ImageButton
 import android.widget.TextView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
-import kotlinx.android.synthetic.main.activity_transaction_detail.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,6 +23,11 @@ class TransactionDetailActivity : AppCompatActivity() {
 
         var transaction = intent.getParcelableExtra<TransactionRowItem>("transaction")
 
+        var btnBack = findViewById<ImageButton>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            super.finish()
+        }
+
         var txtAmount = findViewById<TextView>(R.id.transaction_detail_amount)
         var txtCategory = findViewById<TextView>(R.id.transaction_detail_category)
         var recycleView = findViewById<RecyclerView>(R.id.recycler_view_transaction_detail)
@@ -32,6 +37,13 @@ class TransactionDetailActivity : AppCompatActivity() {
 
 
         val adapter = GroupAdapter<ViewHolder>()
+
+        // android:background="@drawable/custom_transaction_detail_income"
+        val container = findViewById<ConstraintLayout>(R.id.transaction_detail_category_amount)
+        if (transaction.transactionType == "expense")
+            container.background = getDrawable(R.drawable.custom_transaction_detail_expense)
+        else
+            container.background = getDrawable(R.drawable.custom_transaction_detail_income)
 
         val date = getFormattedDate(transaction.date)
         adapter.add(TransactionDetailItem("Data",date.toString()))
@@ -43,8 +55,6 @@ class TransactionDetailActivity : AppCompatActivity() {
 
         recycleView.adapter = adapter
         recycleView.addItemDecoration(DividerItemDecoration(this,DividerItemDecoration.VERTICAL))
-
-
     }
 
     private fun getFormattedDate(date: String): Any {
