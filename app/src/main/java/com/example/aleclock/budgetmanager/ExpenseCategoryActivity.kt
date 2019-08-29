01a71fab3300989805f9d7370c48867fad96818f
“Alessandro <clocchiatti.alessandro@educ.di.unito.it>
2019-08-29 12:47:23 +0200

    Iniziato tab setting, aggiunta listview con item "entrate","uscite","lingua. Creato activity per i primi due (expenseActivity e incomeActivitY)

---
+++ app/src/main/java/com/example/aleclock/budgetmanager/ExpenseCategoryActivity.kt
@@ -0,0 +1,20 @@
+package com.example.aleclock.budgetmanager
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ImageButton
+
+class ExpenseCategoryActivity : AppCompatActivity()  {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category_list)
+
+
+        var btnBack = findViewById<ImageButton>(R.id.btn_back)
+        btnBack.setOnClickListener {
+            super.finish()
+        }
+    }
+
+}
--- app/src/main/java/com/example/aleclock/budgetmanager/SettingsFragment.kt
+++ app/src/main/java/com/example/aleclock/budgetmanager/SettingsFragment.kt
@@ -5,10 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
+import com.xwray.groupie.OnItemClickListener
+import java.math.RoundingMode.valueOf
 
 
 /**
@@ -29,11 +33,42 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val contact = arrayOf(
+                resources.getString(R.string.category_expense),
+                resources.getString(R.string.category_income),
+                resources.getString(R.string.language))
+
+
+        val listView = view.findViewById<ListView>(R.id.setting_listview)
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(context,android.R.layout.simple_list_item_1,contact)
+        listView.adapter = arrayAdapter
+        listView.setOnItemClickListener { parent, view, position, id ->
+            when(id.toInt()) {
+                0 -> {
+                    val intent = Intent(context,ExpenseCategoryActivity::class.java)
+                    startActivity(intent)
+                }
+                1 -> {
+                    val intent = Intent(context,ExpenseCategoryActivity::class.java)
+                    startActivity(intent)
+                }
+                2 -> {
+                    Log.d("aaa","Cambia lista")
+                }
+            }
+        }
+
         /**
          * Logout
          */
         val btnLogout = view!!.findViewById<Button>(R.id.btn_logout)
         btnLogout!!.setOnClickListener { logoutUser() }
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+
+        Log.d("onContextItemSelected", item.toString())
+        return super.onContextItemSelected(item)
     }
 
     private fun logoutUser() {
@@ -45,3 +80,4 @@ class SettingsFragment : Fragment() {
         startActivity(intent)
     }
 }
+
